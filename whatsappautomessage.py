37e9fd1d6c01db3f3b1c0df09340db8f9562242b
rufatsamadov <63500334+rufatsamadov@users.noreply.github.com>
2021-09-12 23:18:46 +0400

    Update whatsappautomessage.py

--- whatsappautomessage.py
+++ whatsappautomessage.py
@@ -5,29 +5,29 @@ import pandas as pd
 
 data = pd.read_csv("numbers.csv")
 data_dict = data.to_dict('list')
-leads = data_dict['Numara']
-messages = data_dict['Mesaj']
+leads = data_dict['Number']
+messages = data_dict['Message']
 combo = zip(leads,messages)
 first=True
-zaman=input("Mesaji gondermek istedigin zamani yaz (mesela-12:00:31)>>>")
+sending_time=input("Enter the time that you want to send message (ex. 12:00:31)>>>")
 
 def type():
     for lead,message in combo:
-        time.sleep(4)
+        time.sleep(3)
         web.open("https://web.whatsapp.com/send?phone="+lead+"&text="+message)
         if first:
-            time.sleep(6)
+            time.sleep(3)
             first=False
         width,height = pg.size()
-        time.sleep(5)
+        time.sleep(3)
         pg.click(width/2,height/2)
-        time.sleep(8)
+        time.sleep(1)
         pg.press('enter')
-        time.sleep(8)
+        time.sleep(3)
         pg.hotkey('ctrl', 'w')
 while True:
     t = time.localtime()
-    simdiki_zaman = time.strftime("%H:%M:%S", t)
-    if __name__=="__main__" and simdiki_zaman==zaman :
+    current_time = time.strftime("%H:%M:%S", t)
+    if  current_time==sending_time :
         type()
     continue
